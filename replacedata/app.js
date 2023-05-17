5bc8992947275cd98b1291ce2e68966fe159396c
André Rauber <andrerauber2006@gmail.com>
2023-05-17 20:37:29 -0300

    att replacedata
    product id and api on the server

--- replacedata/app.js
+++ replacedata/app.js
@@ -9,6 +9,8 @@ const dataObj = JSON.parse(data);
 
 const temphome = fs.readFileSync(`${__dirname}/temp/home.html`, 'utf-8')
 const tempcontent = fs.readFileSync(`${__dirname}/temp/content.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/temp/product.html`, 'utf-8')
+
 
  
 const server = http.createServer((req, res) => {
@@ -22,6 +24,32 @@ const server = http.createServer((req, res) => {
         const output = temphome.replace('{%CONTENT%}', replacecontent);
         res.end(output);
     }
+
+    else if (pathname === '/product'){
+        res.writeHead(200, {
+            'content-type' : 'text/html'
+        })
+        const product = dataObj[query.id]
+        const output = replacedata(tempProduct, product)
+        res.end(output);
+    }
+
+    else if (pathname === '/api'){
+        res.writeHead(200, {
+            'content-type' : 'text/json'
+        })
+        res.end(data)
+    }
+
+    else {
+        res.writeHead(404, {
+            'content-type': 'text/html',
+            'erro' :'page-not-found'
+        })
+        res.end('<h1>Page Not Found 😡 </h1>')
+    }
+
+
 })
 
 server.listen(8000, () => {
